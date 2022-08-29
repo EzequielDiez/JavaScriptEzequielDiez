bc8e112098a55d64ff8e98509db3a5f12f2cdaf2
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2022-08-29 00:07:40 -0300

    Function Plazo Fijo Tradicional

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let nombre = prompt("Ingrese su nombre")
 let apellido = prompt("Ingrese su apellido")
 alert("Bienvenido/a" + " " + nombre + " " + apellido)
 
-//TABLA MULTIPLICACIóN
+/* TABLA MULTIPLICACIóN
 
 let numero = parseInt(prompt("Ingrese un número para conocer su multiplicación:"))
 let multiplicador = parseInt(prompt("Ingrese el máximo multiplicador deseado"))
@@ -12,3 +12,29 @@ for (let i = 1; i <= multiplicador; i++) {
     let resultado = numero * i;
     console.log (numero +" X "+ i + "="+ resultado)
 }
+ */
+
+
+// Simulador plazo fijo tradicional
+
+//Ingreso datos
+
+let capitalinicial = parseInt(prompt("Ingrese el capital a invertir (solo números)"))
+    console.log ("Capital Inicial: ", capitalinicial)
+let tna = 69.5/100
+    console.log ("Tasa: ", tna*100 + "%")
+let duracion = parseInt(prompt("Ingrese duración de la inversión (mayor a 30 días)"))
+    console.log ("Duración: ", duracion + " días")
+
+//Calculo Plazo Fijo
+
+function calcularPlazofijo() {
+    if(capitalinicial !== " " && tna !== " " && duracion >= 30){
+    let capitalfinal = capitalinicial * (1 + tna * (duracion / 365))
+        console.log ("Intereses ganados: ", capitalfinal - capitalinicial)
+        console.log ("Capital final: ", capitalfinal)
+    } else
+        console.error ("Usted ingreso una duracion menor a 30 días")
+}
+
+calcularPlazofijo();
